ef4a85ac4631c0b24216c69232ecf2d68001f2b5
Alina Pak <alinavpak@gmail.com>
2019-12-02 21:15:58 -0800

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow';
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, quas quam magni eius reiciendis in nam provident, totam repellendus similique voluptatem. Nihil reprehenderit debitis maxime nulla! Quas quia accusamus illo!</p>
+        </div>
+    )
+}
+
+export default Rainbow(About);
---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = (props) => {
+    // setTimeout(() => {
+    //     props.history.push('/about')
+    // }, 2000);
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, quas quam magni eius reiciendis in nam provident, totam repellendus similique voluptatem. Nihil reprehenderit debitis maxime nulla! Quas quia accusamus illo!</p>
+        </div>
+    )
+}
+
+export default Contact;
