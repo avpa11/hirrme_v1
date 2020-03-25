a4865abef516d014ee75555be0f00b1185cf0b38
Alina <alinavpak@gmail.com>
2020-03-25 00:14:55 -0700

    firebase config with env

--- src/components/FirebaseConnection.js
+++ src/components/FirebaseConnection.js
@@ -2,29 +2,30 @@ import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/analytics';
-import 'firebase/storage'
+import 'firebase/storage';
+require('dotenv').config();
 
 const firebaseConfig = {
-          apiKey: "AIzaSyBrd47aC5DMfPk2D3_ejrFD9uqi0GQ1m30",
-          authDomain: "hirr-12.firebaseapp.com",
-          databaseURL: "https://hirr-12.firebaseio.com",
-          projectId: "hirr-12",
-          storageBucket: "hirr-12.appspot.com",
-          messagingSenderId: "499108259977",
-          appId: "1:499108259977:web:dccb6a56b42862256b7e58",
-          measurementId: "G-QNL3PHTCVC"
+          apiKey: process.env.REACT_APP_API_KEY,
+          authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+          databaseURL: process.env.REACT_APP_DATABASE_URL,
+          projectId: process.env.REACT_APP_PROJECT_ID,
+          storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+          messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+          appId: process.env.REACT_APP_APP_ID,
+          measurementId: process.env.REACT_APP_MEASUREMENT_ID
       };
 
 
 const firebaseConfigProduction = {
-          apiKey: "AIzaSyAtvGKxO5slXwpjqdkbnHqkG201uonx2Ak",
-          authDomain: "hirrproduction.firebaseapp.com",
-          databaseURL: "https://hirrproduction.firebaseio.com",
-          projectId: "hirrproduction",
-          storageBucket: "hirrproduction.appspot.com",
-          messagingSenderId: "1009908311945",
-          appId: "1:1009908311945:web:acac06697fe9cd6e052358",
-          measurementId: "G-ZE85J52CFR"
+    apiKey: process.env.REACT_APP_API_KEY_PROD,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN_PROD,
+    databaseURL: process.env.REACT_APP_DATABASE_URL_PROD,
+    projectId: process.env.REACT_APP_PROJECT_ID_PROD,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET_PROD,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID_PROD,
+    appId: process.env.REACT_APP_APP_ID_PROD,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID_PROD
       };
 
 const config = process.env.NODE_ENV === 'production' ? firebaseConfigProduction : firebaseConfig;
